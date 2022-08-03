b6368c54a061aad7a9f29147ddcef14bfa3e9187
muneeramm <muneeraabbas22@gmail.com>
2022-08-04 00:09:36 +0530

    order and orderitem in db

--- ecommerce/core/admin.py
+++ ecommerce/core/admin.py
@@ -6,3 +6,5 @@ from core.models import *
 admin.site.register(Customer)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(OrderItem)
+admin.site.register(Order)
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import render,redirect
 from core.forms import *
-
+from core.models import *
+from django.utils import timezone
 # Create your views here.
 def index(request):
     # to get all products from database table
@@ -33,3 +34,33 @@ def product_desc(request,pk):
     # Get that particular product of id = pk
     product = Product.objects.get(pk=pk)
     return render(request,'core/product_desc.html',{'product':product})
+
+def add_to_cart(request,pk):
+    # Get that particular product of id = pk
+    product = Product.objects.get(pk=pk)
+    # Create Order item
+    order_item, created = OrderItem.objects.get_or_create(
+        product = product,
+        user = request.user,
+        ordered = False,
+    )
+
+    # Get query set of order object of particular user
+    order_qs = Order.objects.filter(user=request.user,ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(product__pk = pk).exists():
+            order_item.quantity +=1
+            order_item.save()
+            messages.info(request,"Added quantity item")
+            return redirect("product_desc",pk=pk)
+        else:
+            order.items.add(order_item)
+            messages.info(request,"Item added to cart")
+            return redirect("product_desc",pk=pk)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user,ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request,"Item added to cart")
+        return redirect("product_desc",pk=pk)
