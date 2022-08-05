e1e043e76b95ee9b6e265fba222906936a98e3e4
muneeramm <muneeraabbas22@gmail.com>
2022-08-05 12:20:07 +0530

    modif cart and did orderlist

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('add_product',views.add_product,name='add_product'),
     path('product_desc/<pk>',views.product_desc,name="product_desc"),
     path('add_to_cart/<pk>',views.add_to_cart,name="add_to_cart"),
+    path('orderlist',views.orderlist,name="orderlist"),
+    path('add_item/<int:pk>',views.add_item,name="add_item"),
+    path('remove_item/<int:pk>',views.remove_item,name="remove_item"),
 
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,5 +1,6 @@
+from itertools import product
 from django.contrib import messages
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
 from core.forms import *
 from core.models import *
 from django.utils import timezone
@@ -8,9 +9,7 @@ def index(request):
     # to get all products from database table
     products = Product.objects.all()
     return render(request,"core/index.html",{'products':products})
-
     
-
 def add_product(request):
     if request.method == 'POST':
         form=ProductForm(request.POST,request.FILES)
@@ -20,7 +19,6 @@ def add_product(request):
     
             print("Data saved successfully")
             messages.success(request,"Product added successfully")
-
             return redirect('/')
         else:
             print("Not working")
@@ -64,3 +62,71 @@ def add_to_cart(request,pk):
         order.items.add(order_item)
         messages.info(request,"Item added to cart")
         return redirect("product_desc",pk=pk)
+
+def orderlist(request):
+    if Order.objects.filter(user=request.user,ordered=False).exists():
+        order = Order.objects.get(user = request.user,ordered=False)
+        return render(request,'core/orderlist.html',{'order':order})
+    return render(request,'core/orderlist.html',{'message':"Your cart is empty"})
+
+def add_item(request,pk):
+    # Get that particular product of id = pk
+    product = Product.objects.get(pk=pk)
+    # Create Order item
+    order_item, created = OrderItem.objects.get_or_create(
+        product = product,
+        user = request.user,
+        ordered = False,
+    )
+
+    # Get query set of order object of particular user
+    order_qs = Order.objects.filter(user=request.user,ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(product__pk = pk).exists():
+            if order_item.quantity < product.product_available_count:
+                order_item.quantity +=1
+                order_item.save()
+                messages.info(request,"Added quantity item")
+                return redirect("orderlist")
+            else:
+                messages.info(request,"Sorry! Product is out of stock")
+                return redirect("orderlist")
+        else:
+            order.items.add(order_item)
+            messages.info(request,"Item added to cart")
+            return redirect("product_desc",pk=pk)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user,ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request,"Item added to cart")
+        return redirect("product_desc",pk=pk)
+
+def remove_item(request,pk):
+    item = get_object_or_404(Product,pk=pk)
+    order_qs = Order.objects.filter(
+        user = request.user,
+        ordered = False,
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(product__pk=pk).exists():
+            order_item = OrderItem.objects.filter(
+                product=item,
+                user = request.user,
+                ordered = False,
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order_item.delete()
+            messages.info(request,"Item quantity was updated")
+            return redirect("orderlist")
+        else:
+            messages.info(request,"This item is not in your cart")
+            return redirect("orderlist")
+    else:
+        messages.info(request,"You do not have any order")
+        return redirect("orderlist")
